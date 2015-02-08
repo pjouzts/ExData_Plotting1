7a6eeb24a759034c95dd28500797732c8c39524b
pjouzts <pjouzts@gmail.com>
2015-02-08 12:30:12 -0500

    CP1
    Signed-off-by: pjouzts <pjouzts@gmail.com>

---
+++ plot2.R
@@ -0,0 +1,48 @@
+plot2 <- function(){
+  
+  ## function to plot data from electric consumption
+  
+  ## check if file exists
+  if (!file.exists("./data/household_power_consumption.txt")){
+    
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata_data_household_power_consumption.zip"
+    download.file(fileUrl, destfile = "./data/exdata_data_household_power_consumption.zip")
+    ## need to issue command to unzip but don't know that
+  }
+    
+  ## read in the text file, this is the whole file
+  fileName <- "./data/household_power_consumption.txt"
+  data <- read.table(fileName, sep=";", header=TRUE, stringsAsFactor = FALSE)
+  
+  ## extract only the dates needed
+  ## this using a sql query to filter as described in class forums
+  ##dataDates <- read.csv.sql("household_power_consumption.txt",header=TRUE,sep=";",
+  ##                     sql="Select * from file where Date = '1/1/2007' OR Date = '2/1/2007'")
+  
+  dataDates <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
+  
+  ## convert to data frame (not sure if this is necessary)
+  dataDatesDF <- as.data.frame(dataDates)
+  
+  ## convert date/time to POSIX
+  dateStrp <- strptime(paste(dataDatesDF$Date,dataDatesDF$Time),
+                       "%d/%m/%Y %H:%M:%S")
+  
+  ## paste the new data/time object into the end of the existing data
+  dataDatesDF_append <- cbind(dataDatesDF, dateStrp)
+
+  ## now plot this data, with no data symbols
+  plot(dataDatesDF_append$dateStrp,as.numeric(dataDatesDF_append$Global_active_power)/1000.,
+       type="n",
+       xlab = "",
+       ylab ="Global Active Power (kilowatts)")
+  
+  ## now add the lines, note convert power to kW
+  lines(dataDatesDF_append$dateStrp,as.numeric(dataDatesDF_append$Global_active_power)/1000.)
+
+  ## save plot to png file
+  dev.copy(png,filename="plot2.png")
+  dev.off()
+
+}
+                                                                                                      
